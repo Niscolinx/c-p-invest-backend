a2cdad43dbcc06c53f25084bb66923e8ecd959de
Igboanugwo Collins <munisco12@gmail.com>
2020-09-13 20:45:30 +0100

    update

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ app.use((error, req, res, next) => {
 console.log('the env process', process.env.NODE_ENV)
 
 const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3030
 
 mongoose
     .connect(
