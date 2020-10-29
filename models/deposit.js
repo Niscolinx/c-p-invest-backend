fda2cbe4b9845f023d19198714a9a6a08a76ea6e
Igboanugwo Collins <munisco12@gmail.com>
2020-10-29 21:33:26 +0100

    added more schemas

---
+++ models/deposit.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const depositSchema = new Schema(
+    {
+        amount: {
+            type: String,
+            required: true,
+        },
+        packageName: {
+            type: String,
+            required: true,
+        },
+        currency: {
+            type: String,
+            required: true,
+        },
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+            required: true,
+        },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('deposit', depositSchema)
