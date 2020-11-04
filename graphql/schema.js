88b2a9b3beafdf00a6108765f33fdc5c955c8b81
Igboanugwo Collins <munisco12@gmail.com>
2020-11-04 15:59:58 +0100

    Working on withdrawal

--- graphql/schema.js
+++ graphql/schema.js
@@ -107,7 +107,7 @@ module.exports = buildSchema(`
         createUser(userData: UserInputData): User!
         createFundAccount(fundData: PostFundData): FundAccount!
         createInvestNow(investNowData: PostInvestNowData): FundAccount!
-        createWithdrawNow(withdrawNowData: PostwithdrawNowData): FundAccount!
+        createWithdrawNow(withdrawNowData: PostWithdrawNowData): FundAccount!
         createUpdateProfile(updateProfileData: PostProfileData): ProfileData!
         createFundAccountApproval(PostId: PostId): FundAccount!
         createInvestNowApproval(PostId: PostId): FundAccount!
